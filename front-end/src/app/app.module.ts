6b4de15afa9741789a671f2567dc2217cebfbfa5
OrlandoBG <orlandopsn1@gmail.com>
2023-02-03 17:32:15 -0300

    Salvando contato no banco a partir do formulario

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContatosService } from './contatos.service';
 import { ContatosComponent } from './contatos/contatos.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +29,12 @@ import { ContatosComponent } from './contatos/contatos.component';
     HttpClientModule,
     MatButtonModule,
     MatInputModule,
+    MatCardModule,
     MatToolbarModule,
+    MatTabsModule,
+    MatIconModule,
+    MatTableModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule
   ],
   providers: [
--- front-end/src/app/contatos/contatos.component.ts
+++ front-end/src/app/contatos/contatos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { ContatosService } from '../contatos.service';
 import { Contato } from './contato';
+import { FormBuilder, FormGroup} from '@angular/forms'
 
 @Component({
   selector: 'app-contatos',
@@ -9,11 +10,30 @@ import { Contato } from './contato';
 })
 export class ContatosComponent {
 
-  constructor(private service: ContatosService){
+  formulario!: FormGroup;
+  contatos: Contato[]=[];
+
+  constructor(private service: ContatosService, private fb: FormBuilder){
 
   }
 
   ngOnInit(){
+    this.formulario = this.fb.group({
+      nome: [''],
+      email: ['']
+    })
+  }
+
+
+  submit(){
+
+    const formValues = this.formulario.value;
+    const contato = new Contato(formValues.nome, formValues.email);
+    this.service.salvar(contato)
+                .subscribe(response =>{
+                    this.contatos.push(response);
+                    console.log(this.contatos);
+                })
     
   }
 
